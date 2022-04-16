94ac9d2e69f55acf1278a7e969f24d42b7ef8123
Rafael Medeiros Ribeiro <rafaelmedrib@gmail.com>
2022-04-16 09:24:59 -0300

    :tada: :sparkles: initial commit; adds middleware + create new user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const res = require('express/lib/response');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(400).json({
+      error: "Usuário não encontrado"
+    });
+  }
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  
+  const usernameAlreadyExists = users.some(user => user.username === username);
+  if(usernameAlreadyExists) {
+    return response.status(400).json({message: "Username already exists"});
+  }
+  
+  const newUser = {
+    id: uuidv4(),
+    name: name,
+    username: username,
+    todos: []
+  }
+
+  users.push(newUser);
+  return response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
